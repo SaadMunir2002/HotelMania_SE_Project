f2eacc3d10d391717c14c4a8147d78f2711f4333
Muhammad Naeem Shah <mahwishnoreen2005@gmail.com>
2023-11-23 22:58:36 +0500

    generatebill

---
+++ BillGeneration.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace finalsProject
+{
+    partial class BillGeneration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BillGeneration));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.backbtn = new System.Windows.Forms.PictureBox();
+            this.printbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountpaidtxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.orderidcombo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.backbtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(456, 522);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.backbtn);
+            this.panel2.Controls.Add(this.printbtn);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.amountpaidtxt);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.orderidcombo);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(456, 522);
+            this.panel2.TabIndex = 0;
+            // 
+            // backbtn
+            // 
+            this.backbtn.BackColor = System.Drawing.Color.Transparent;
+            this.backbtn.Image = global::finalsProject.Properties.Resources._1212;
+            this.backbtn.ImageLocation = "";
+            this.backbtn.Location = new System.Drawing.Point(0, 0);
+            this.backbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.backbtn.Name = "backbtn";
+            this.backbtn.Size = new System.Drawing.Size(47, 46);
+            this.backbtn.TabIndex = 58;
+            this.backbtn.TabStop = false;
+            this.backbtn.Click += new System.EventHandler(this.backbtn_Click);
+            // 
+            // printbtn
+            // 
+            this.printbtn.BackColor = System.Drawing.Color.Pink;
+            this.printbtn.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printbtn.Location = new System.Drawing.Point(326, 11);
+            this.printbtn.Name = "printbtn";
+            this.printbtn.Size = new System.Drawing.Size(120, 36);
+            this.printbtn.TabIndex = 14;
+            this.printbtn.Text = "Print";
+            this.printbtn.UseVisualStyleBackColor = false;
+            this.printbtn.Click += new System.EventHandler(this.printbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "0.00";
+            // 
+            // amountpaidtxt
+            // 
+            this.amountpaidtxt.Location = new System.Drawing.Point(22, 447);
+            this.amountpaidtxt.Name = "amountpaidtxt";
+            this.amountpaidtxt.Size = new System.Drawing.Size(175, 20);
+            this.amountpaidtxt.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 470);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Amount Returned";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(19, 421);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Amount Paid";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(294, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = " Show Bill";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(81, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0.00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bill : ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Goldenrod;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 225);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Details";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // orderidcombo
+            // 
+            this.orderidcombo.FormattingEnabled = true;
+            this.orderidcombo.Location = new System.Drawing.Point(39, 87);
+            this.orderidcombo.Name = "orderidcombo";
+            this.orderidcombo.Size = new System.Drawing.Size(234, 21);
+            this.orderidcombo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Order ID";
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.document);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Pink;
+            this.button2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(178, 482);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 36);
+            this.button2.TabIndex = 59;
+            this.button2.Text = "Generate  Bill";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BillGeneration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 518);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BillGeneration";
+            this.Text = "BillGeneration";
+            this.Load += new System.EventHandler(this.BillGeneration_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.backbtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox orderidcombo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox amountpaidtxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button printbtn;
+        private System.Windows.Forms.PictureBox backbtn;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ BillGeneration.cs
@@ -0,0 +1,132 @@
+﻿using finalsProject.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalsProject
+{
+    public partial class BillGeneration : Form
+    {
+        List<int> l = new List<int>();
+        public BillGeneration()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BillGeneration_Load(object sender, EventArgs e)
+        {
+            ShowIcon = false;
+            Text = "";
+            orderidcombo.Items.Clear();
+            l = OrderDL.retriveorderids();
+            for (int i = 0; i < l.Count; i++)
+            {
+
+                orderidcombo.Items.Add(l[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (orderidcombo.Text != "")
+                {
+                    // dataGridView1.AutoGenerateColumns = false;
+                    DataTable d = OrderItemsDL.retriveorderitem(int.Parse(orderidcombo.Text));
+                    dataGridView1.DataSource = d;
+                    int bill = OrderItemsDL.generatecustbill(int.Parse(orderidcombo.Text));
+                    label5.Text = bill.ToString();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("select orderid");
+                }
+
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+          
+        }
+
+        private void backbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form f = new ManagerMenu();
+            f.Show();
+        }
+        Bitmap bitmap;
+        private void printbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int height = dataGridView1.Height;
+                dataGridView1.Height = dataGridView1.RowCount * dataGridView1.RowTemplate.Height * 2;
+                bitmap = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+                dataGridView1.DrawToBitmap(bitmap, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+                printPreviewDialog1.PrintPreviewControl.Zoom = 1;
+                printPreviewDialog1.ShowDialog();
+                dataGridView1.Height = height;
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+           
+        }
+
+      
+        private void printPreviewDialog1_Load_1(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmap, 0, 0);
+        }
+
+        private void document(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(amountpaidtxt.Text != "")
+            {
+                int a = int.Parse(amountpaidtxt.Text);
+                int b = Convert.ToInt32(label5.Text);
+                if (a >= b)
+                {
+                    int c = -(Convert.ToInt32(label5.Text) - int.Parse(amountpaidtxt.Text));
+                    label1.Text = c + " Rs";
+                }
+                else
+                {
+                    MessageBox.Show("amount Paid not added correctly");
+                }
+            }
+            else
+            {
+                MessageBox.Show(" Add amount to be Paid ");
+            }
+            
+           
+        }
+    }
+}
