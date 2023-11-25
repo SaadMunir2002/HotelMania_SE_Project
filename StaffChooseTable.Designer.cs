85a4cdd73f1de3d22d42c51553a7b8ea83634928
Muhammad Naeem Shah <mahwishnoreen2005@gmail.com>
2023-11-25 23:38:58 +0500

    staffchoosetable

---
+++ StaffChooseTable.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace finalsProject
+{
+    partial class StaffChooseTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableidcombo = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Proceedbtn = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.backbtn = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableidcombo)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.backbtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.49484F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.01031F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.49484F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableidcombo, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.backbtn, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.545455F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.03883F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.03883F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.825243F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(371, 179);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Goldenrod;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(64, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 44);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "Table Id";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableidcombo
+            // 
+            this.tableidcombo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableidcombo.DropDownWidth = 273;
+            this.tableidcombo.Items.AddRange(new object[] {
+            "waiter",
+            "deliveryman"});
+            this.tableidcombo.Location = new System.Drawing.Point(64, 61);
+            this.tableidcombo.Name = "tableidcombo";
+            this.tableidcombo.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue;
+            this.tableidcombo.Size = new System.Drawing.Size(242, 38);
+            this.tableidcombo.StateCommon.ComboBox.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.tableidcombo.StateCommon.ComboBox.Border.Rounding = 25;
+            this.tableidcombo.StateCommon.ComboBox.Border.Width = 1;
+            this.tableidcombo.StateCommon.ComboBox.Content.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableidcombo.TabIndex = 62;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 152F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel2.Controls.Add(this.Proceedbtn, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(63, 111);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(244, 53);
+            this.tableLayoutPanel2.TabIndex = 64;
+            // 
+            // Proceedbtn
+            // 
+            this.Proceedbtn.Location = new System.Drawing.Point(47, 3);
+            this.Proceedbtn.Name = "Proceedbtn";
+            this.Proceedbtn.OverrideDefault.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.OverrideDefault.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Proceedbtn.OverrideDefault.Back.ColorAngle = 45F;
+            this.Proceedbtn.OverrideDefault.Back.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.Proceedbtn.OverrideDefault.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.OverrideDefault.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Proceedbtn.OverrideDefault.Border.ColorAngle = 45F;
+            this.Proceedbtn.OverrideDefault.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.OverrideDefault.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.Proceedbtn.OverrideDefault.Border.Rounding = 25;
+            this.Proceedbtn.OverrideDefault.Border.Width = 1;
+            this.Proceedbtn.OverrideFocus.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.OverrideFocus.Back.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.OverrideFocus.Back.ColorAngle = 45F;
+            this.Proceedbtn.OverrideFocus.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.OverrideFocus.Border.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.OverrideFocus.Border.ColorAngle = 45F;
+            this.Proceedbtn.OverrideFocus.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.OverrideFocus.Border.Rounding = 25;
+            this.Proceedbtn.OverrideFocus.Border.Width = 1;
+            this.Proceedbtn.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.Proceedbtn.Size = new System.Drawing.Size(146, 46);
+            this.Proceedbtn.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateCommon.Back.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StateCommon.Back.ColorAngle = 45F;
+            this.Proceedbtn.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateCommon.Border.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StateCommon.Border.ColorAngle = 45F;
+            this.Proceedbtn.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.Proceedbtn.StateCommon.Border.Rounding = 25;
+            this.Proceedbtn.StateCommon.Border.Width = 1;
+            this.Proceedbtn.StateCommon.Content.ShortText.ColorAngle = 45F;
+            this.Proceedbtn.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Proceedbtn.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Proceedbtn.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateNormal.Border.Color1 = System.Drawing.Color.DarkKhaki;
+            this.Proceedbtn.StateNormal.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateNormal.Border.ColorAngle = 45F;
+            this.Proceedbtn.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.StateNormal.Border.Rounding = 25;
+            this.Proceedbtn.StateNormal.Border.Width = 1;
+            this.Proceedbtn.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StatePressed.Back.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StatePressed.Back.ColorAngle = 130F;
+            this.Proceedbtn.StatePressed.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StatePressed.Border.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StatePressed.Border.ColorAngle = 130F;
+            this.Proceedbtn.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.StatePressed.Border.Rounding = 25;
+            this.Proceedbtn.StatePressed.Border.Width = 1;
+            this.Proceedbtn.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateTracking.Back.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StateTracking.Back.ColorAngle = 45F;
+            this.Proceedbtn.StateTracking.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Proceedbtn.StateTracking.Border.Color2 = System.Drawing.Color.Yellow;
+            this.Proceedbtn.StateTracking.Border.ColorAngle = 45F;
+            this.Proceedbtn.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Proceedbtn.StateTracking.Border.Rounding = 25;
+            this.Proceedbtn.StateTracking.Border.Width = 1;
+            this.Proceedbtn.TabIndex = 63;
+            this.Proceedbtn.Values.Text = "Proceed";
+            this.Proceedbtn.Click += new System.EventHandler(this.Proceedbtn_Click);
+            // 
+            // backbtn
+            // 
+            this.backbtn.BackColor = System.Drawing.Color.Transparent;
+            this.backbtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backbtn.Image = global::finalsProject.Properties.Resources._1212;
+            this.backbtn.ImageLocation = "";
+            this.backbtn.Location = new System.Drawing.Point(2, 10);
+            this.backbtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.backbtn.Name = "backbtn";
+            this.backbtn.Size = new System.Drawing.Size(57, 40);
+            this.backbtn.TabIndex = 65;
+            this.backbtn.TabStop = false;
+            this.backbtn.Click += new System.EventHandler(this.backbtn_Click);
+            // 
+            // StaffChooseTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(371, 179);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "StaffChooseTable";
+            this.Text = "StaffChooseTable";
+            this.Load += new System.EventHandler(this.StaffChooseTable_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableidcombo)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.backbtn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonComboBox tableidcombo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton Proceedbtn;
+        private System.Windows.Forms.PictureBox backbtn;
+    }
+}
---
+++ StaffChooseTable.cs
@@ -0,0 +1,55 @@
+﻿using finalsProject.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalsProject
+{
+    public partial class StaffChooseTable : Form
+    {
+        int staffid = SigninOthers.s.Id;
+        static public int orderid;
+        public StaffChooseTable()
+        {
+            InitializeComponent();
+        }
+
+        private void StaffChooseTable_Load(object sender, EventArgs e)
+        {
+            tableidcombo.Items.Clear();
+            List<int> l =  StaffDL.viewassignedtables(staffid);
+            for(int i = 0; i <l.Count; i++)
+            {
+                tableidcombo.Items.Add(l[i]);
+            }
+        }
+        private void Proceedbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Order o = new Order(staffid, int.Parse(tableidcombo.Text));
+                OrderDL.StaffChooseTable(o);
+                orderid = o.Orderid;
+                this.Hide();
+                Form f = new staffTakeOrders();
+                f.Show();
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+          
+        }
+
+        private void backbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
